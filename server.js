7ba9481973dc7453b6f3b0b7716be75fcae617b3
jessicakwalters <jessicakwalters@gmail.com>
2019-07-23 09:02:18 -0700

    Set up initial route

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 'use strict'
 
+//App Dependencies
 const express = require('express');
 const superagent = require('superagent');
 
+//App Setup
 const app = express();
 const PORT = process.env.PORT || 3000
 
+//App Middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+//View engine
+app.set('view engine', 'ejs');
+
+//API ROUTES
+
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+
+//Liste
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+});
